ef7c7ff630810e13436588501f1b1a71bea7b996
kieo23 <dankstarflint@gmail.com>
2025-08-07 17:57:45 +0900

    1차 로그인/ 회원가입구현

---
+++ src/main/java/com/woori/codenova/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.woori.codenova;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+//클래스 레벨 타입의 시큐리티
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+	@Bean // 현재 모든 페이지 권한을 설정하였으나 "사용자"에겐 주지 않은 상황
+	// 403 error 발생
+	// 모든 페이지에 접근 가능
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+				.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+				// DB나 특정링크 접속권한을 주지만 내부 데이터 접근불가
+				// 즉 로그인창만 넘어가며 나머진 거부됨
+				.csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+				// h2-console을 보안에서 허용한다.
+				// 즉 접근경로가 h2-console 뿐인데 그걸로 접속하는걸 허용한다
+				.headers((headers) -> headers.addHeaderWriter(
+						new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+				// 로그인 페이지 -> /user/login , 로그인 성공시 -> / 로 돌아간다
+				.formLogin((formLogin) -> formLogin.loginPage("/user/login").defaultSuccessUrl("/"))
+				.logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+						.logoutSuccessUrl("/").invalidateHttpSession(true));
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	// 인증과 인가(권한) 부여를 처리함
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+}
---
+++ src/main/java/com/woori/codenova/controller/UserController.java
@@ -0,0 +1,67 @@
+package com.woori.codenova.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.woori.codenova.UserCreateForm;
+import com.woori.codenova.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	private final UserService userService;
+
+	// 회원가입 링크로 보내버림
+	@GetMapping("/signup")
+	public String signup(UserCreateForm userCreateForm) {
+		return "signup_form";
+	}
+
+	@PostMapping("/signup")
+	public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+
+		// 유효성 오류 발생시 돌려보냄
+		if (bindingResult.hasErrors()) {
+			return "signup_form";
+		}
+		// 비밀번호와 비번확인 검증 시나리오
+		if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+			bindingResult.rejectValue("password2", "passwordInCorrect", "2개의 패스워드가 일치하지 않습니다.");
+			return "signup_form";
+		}
+
+		// 서비스 계층 호출하려다남은 잔재
+//		userService.createUserTest(userCreateForm.getUserid(), userCreateForm.getEmail());
+
+		try {
+			userService.create(userCreateForm.getUserId(), userCreateForm.getPassword1(), userCreateForm.getEmail());
+			// 서비스 계층에서 중복사용자 걸러내는 로직 추가해보았음
+//	} catch (DataIntegrityViolationException e) {
+//			e.printStackTrace();
+//			bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+//			return "signup_form";
+		} catch (Exception e) {
+			e.printStackTrace();
+			bindingResult.reject("singupFailed", e.getMessage());
+			return "signup_form";
+
+		}
+		// 회원가입 끝나면 메인화면에 연결된 곳으로 보내버림
+		return "redirect:/";
+	}
+
+	// 로그인폼으로 보내버림 -> 로그인 처리는 UserSecurityService 에서 처리함
+	@GetMapping("/login")
+	public String login() {
+		return "login_form";
+	}
+
+}
