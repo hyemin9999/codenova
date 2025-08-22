e51ce166bfa04cb37f421255b4ec381a28ab8fc7
dkrdj896 <dkrdj896@gmail.com>
2025-08-22 18:32:39 +0900

    추천 토글 검색 태그 페이징 변경

--- src/test/java/com/woori/codenova/CodenovaApplicationTests.java
+++ src/test/java/com/woori/codenova/CodenovaApplicationTests.java
@@ -7,7 +7,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -160,7 +159,7 @@ class CodenovaApplicationTests {
 	}
 
 	// 테스트 데이터 300개 생성
-	@Test
+	// @Test
 	void testJpa_12() {
 		for (int i = 1; i <= 300; i++) {
 			String subject = String.format("테스트 데이터입니다:[%03d]", i);
