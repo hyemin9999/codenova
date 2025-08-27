fac10ffb296dd7e8f995d4283194fe8ffc90fda6
hyemin9999 <hmlim9999@gmail.com>
2025-08-27 16:42:20 +0900

    관리자 페이지 병합중

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -2,25 +2,35 @@
  * 
  */
 
+const kw = document.getElementById("search_field");
+
+const page = document.getElementById("search_field");
+const field = document.getElementById("search_field");
+const search_field = document.getElementById("search_field");
+const searchForm = document.getElementById('searchForm');
+/**
+ * 검색 버튼 처리
+ */
+
 const search_kw = document.getElementById("search_kw");
 if (search_kw != null) {
 	search_kw.addEventListener('keypress', function() {
 		if (event.keyCode === 13) {
-			document.getElementById('kw').value = this.value;
-			document.getElementById('page').value = 0;
-			document.getElementById('searchForm').submit();
+			kw.value = this.value;
+			page.value = 0;
+			field.value = search_field.value;
+			searchForm.submit();
 		}
 	});
 }
 
-
-
+const ecp1 = document.querySelector('#ecp1');
 const viewerElement = document.querySelector('#viewer');
 if (viewerElement) {
 	//	const viewer = new 
 	toastui.Editor.factory({
 		el: viewerElement,
 		viewer: true,
-		initialValue: document.querySelector('#ecp1').value
+		initialValue: ecp1.value
 	});
 }
