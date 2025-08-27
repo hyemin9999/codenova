77785dc91d21557b4dfb56ac59531c647008fa19
hyemin9999 <hmlim9999@gmail.com>
2025-08-28 02:11:53 +0900

    경고표시 처리하려고 했으나 성공한것도, 실패한것도 있지만 일단 저장

--- src/main/java/com/woori/codenova/InvalidUuidException.java
+++ src/main/java/com/woori/codenova/InvalidUuidException.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 // HTTP 상태 코드 400(Bad Request)을 반환하도록 설정
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidUuidException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
 
 	public InvalidUuidException() {
 		super("유효하지 않은 UUID입니다.");
