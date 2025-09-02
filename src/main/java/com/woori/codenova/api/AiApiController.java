30e8626f18a3b6106ad4a7f2ea68c56acf14373c
hyemin9999 <hmlim9999@gmail.com>
2025-09-02 19:59:40 +0900

    주석친거 살림

--- src/main/java/com/woori/codenova/api/AiApiController.java
+++ src/main/java/com/woori/codenova/api/AiApiController.java
@@ -1,20 +1,26 @@
 package com.woori.codenova.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.woori.codenova.dto.AiDTO;
+import com.woori.codenova.service.AiService;
 
 @RestController
 @RequestMapping("/api/ai")
 public class AiApiController {
-//    private final AiService aiService;
-//
-//    @Autowired
-//    public AiApiController(AiService aiService) {
-//        this.aiService = aiService;
-//    }
-//
-//    @PostMapping("/gemini")
-//    public AiDTO.GeminiResponse askGemini(@RequestBody AiDTO.GeminiRequest request) {
-//        return aiService.askGemini(request);
-//    }
+    private final AiService aiService;
+
+    @Autowired
+    public AiApiController(AiService aiService) {
+        this.aiService = aiService;
+    }
+
+    @PostMapping("/gemini")
+    public AiDTO.GeminiResponse askGemini(@RequestBody AiDTO.GeminiRequest request) {
+        return aiService.askGemini(request);
+    }
 }
